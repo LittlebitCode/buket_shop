b27fcf2a0787b6b1dbe348c69a487ed02dbf1fd5
LittlebitCode <adityaahza0223@gmail.com>
2026-04-20 21:59:13 +0700

    Fix NameError: import auth_login

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
