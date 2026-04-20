c6bc0d2d3d63117df554a9763ae0adb59af627bc
LittlebitCode <adityaahza0223@gmail.com>
2026-04-20 20:29:51 +0700

    Update deployment files (Procfile, WhiteNoise)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn buket_shop.wsgi
--- buket_shop/settings.py
+++ buket_shop/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +73,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
